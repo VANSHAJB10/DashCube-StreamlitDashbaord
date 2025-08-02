8b676d42845b718bd50075503c87643d47b57850
VANSHAJB10 <vanshajbajaj1002@gmail.com>
2025-08-02 23:00:25 +0530

    Setting up page

--- dashboard.py
+++ dashboard.py
@@ -3,4 +3,7 @@ import random
 import duckdb       
 import pandas        
 
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
 st.title("ST Sales Dashboard")
+st.markdown("_Excel to Dashboard-v0.1_")
